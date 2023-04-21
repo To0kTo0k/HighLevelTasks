2f1be025c6707879e5c5e7fc4b6157605a817e0e
gorodovss22pv191 <sergejgorodov096@gmail.com>
2023-04-21 10:19:22 -0400

    Some annotations fix

--- src/main/java/org/example/ConfigBean.java
+++ src/main/java/org/example/ConfigBean.java
@@ -1,11 +1,11 @@
 package org.example;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
+@ImportResource("classpath:xmlBean.xml")
 @Configuration
-@ComponentScan("org.example")
 public class ConfigBean {
     @Bean
     public MisterBean hiBean() {
